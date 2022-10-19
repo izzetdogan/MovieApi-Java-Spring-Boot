ed0209d2cbed1a236caa4b1fde4bd6dc4eddd845
=izzet <dogan.izzet@outlook.com>
2022-10-19 23:02:32 +0300

    Refactor repository

--- src/main/java/com/movie/repository/MovieYearRepository.java
+++ src/main/java/com/movie/repository/MovieYearRepository.java
@@ -3,5 +3,8 @@ package com.movie.repository;
 import com.movie.model.MovieYear;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieYearRepository extends JpaRepository<MovieYear,Integer> {
+    Optional<MovieYear> findByYear(Integer y);
 }
