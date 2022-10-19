cc4e97602ed7f1805d0582464b73bf93743eb1aa
=izzet <dogan.izzet@outlook.com>
2022-10-19 23:05:22 +0300

    Refactor Services

--- src/main/java/com/movie/service/MovieYearService.java
+++ src/main/java/com/movie/service/MovieYearService.java
@@ -1,13 +1,14 @@
 package com.movie.service;
 
 import com.movie.dto.MovieYearDto;
-import com.movie.model.Category;
+import com.movie.exception.ResourceNotFoundException;
 import com.movie.model.MovieYear;
 import com.movie.repository.MovieYearRepository;
+import com.movie.request.MovieYearRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class MovieYearService {
@@ -20,54 +21,38 @@ public class MovieYearService {
 
     //Get All YEar
     public List<MovieYearDto> getAllYear(){
-        List<MovieYear> movieYears = movieYearRepository.findAll();
-        List<MovieYearDto> movieYearDtoList = movieYears.stream()
-                .map((mvy)-> this.toYearDto(mvy)).collect(Collectors.toList());
-        return movieYearDtoList;
-
+        return this.movieYearRepository.findAll().stream().
+                map(MovieYearDto::convert).toList();
     }
 
     // Get Year By ID
     public MovieYearDto getYearById(Integer id){
-        MovieYear movieYear = this.movieYearRepository.findById(id).orElseThrow(()-> new RuntimeException("There is no  id"+id));
-        return this.toYearDto(movieYear);
+        MovieYear movieYear = this.movieYearRepository.findById(id)
+                .orElseThrow( ()-> new ResourceNotFoundException("Movie cannot be found"));
+        return MovieYearDto.convert(movieYear);
     }
 
     //Create Movie Year
-    public MovieYearDto createMovieYear(MovieYearDto movieYearDto){
-        MovieYear movieYear = this.movieYearRepository.save(this.toYear(movieYearDto));
-        MovieYearDto savedOne = this.toYearDto(movieYear);
-        return savedOne;
+    public MovieYearRequest createMovieYear(MovieYearRequest request){
+        MovieYear movieYear = new MovieYear(request.getYear());
+        this.movieYearRepository.save(movieYear);
+        return MovieYearRequest.convert(movieYear);
     }
 
     //Update Movie Year By ID
-    public MovieYearDto updateMovieYear(Integer id, MovieYearDto movieYearDto){
-        MovieYear movieYear = this.movieYearRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no id"+id));
-        movieYear.setYear(movieYearDto.getYear());
-        MovieYear updatedOne = this.movieYearRepository.save(movieYear);
-        return this.toYearDto(updatedOne);
+    public MovieYearRequest updateMovieYear(Integer id, MovieYearRequest request){
+        MovieYear movieYear = this.movieYearRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Year  not found"));
+        MovieYear updatedOne = new MovieYear(movieYear.getId(), request.getYear());
+        this.movieYearRepository.save(updatedOne);
+        return MovieYearRequest.convert(updatedOne);
     }
 
     //Delete MovieYear By ID
     public void deleteMovieYear(Integer id){
-        MovieYear movieYear = this.movieYearRepository.findById(id).orElseThrow(() -> new RuntimeException("ther is no id" +id));
+        MovieYear movieYear = this.movieYearRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Year  not found"));
         this.movieYearRepository.delete(movieYear);
     }
 
-    ///Convert
-
-    private MovieYear toYear(MovieYearDto movieYearDto){
-        MovieYear movieYear = new MovieYear();
-        movieYear.setYear(movieYearDto.getYear());
-        movieYear.setId(movieYearDto.getId());
-        return movieYear;
-
-    }
-
-    private MovieYearDto toYearDto(MovieYear movieYear){
-        MovieYearDto movieYearDto = new MovieYearDto();
-        movieYearDto.setId(movieYear.getId());
-        movieYearDto.setYear(movieYear.getYear());
-        return  movieYearDto;
-    }
 }
